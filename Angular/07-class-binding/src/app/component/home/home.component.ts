1ceac1e27c9dabf31fb900eff19bcda81622dc5b
Parsuram Maharana <123405783+MPARSURAM99@users.noreply.github.com>
2024-10-20 16:12:44 +0530

    class binding

---
+++ Angular/07-class-binding/src/app/component/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  bool: boolean = true;
+}
