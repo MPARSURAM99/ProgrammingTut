82ddceafde65ff1536067fe6135a2a54216a4172
Parsuram Maharana <mparsuram99@gmail.com>
2025-05-07 01:42:20 +0530

    Array datastructure started

---
+++ Java/DataStructure/Array/SearchNumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class SearchNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int arraySize = sc.nextInt();
+        
+        int array[] = new int[arraySize];
+        System.out.println("Enter the elements of the array:");
+
+        for (int i = 0; i < arraySize; i++){
+            array[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements are:");
+        for (int i = 0; i < array.length; i ++) {
+            System.out.print(array[i] + ", ");
+        }
+
+        System.out.println("Enter the number to be searched in the array");
+        int number = sc.nextInt();
+
+        for (int i = 0; i < array.length; i ++) {
+            if (array[i] == number) {
+                System.out.println("The number found at index :" + i);
+            }
+        }
+
+    }
+}
